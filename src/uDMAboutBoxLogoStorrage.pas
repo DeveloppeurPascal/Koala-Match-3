878df01369ff2838c62e6390bcf1f89dd25c9043
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-05-31 12:38:32 +0200

    added the game icon for about box dialog

---
+++ src/uDMAboutBoxLogoStorrage.pas
@@ -0,0 +1,49 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  Koala Match 3
+
+  Copyright 2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  ***************************************************************************
+  File last update : 2025-05-31T12:27:00.000+02:00
+  Signature : 2c973630668fca01fbc7780bf7e48ec0649f9345
+  ***************************************************************************
+*)
+
+unit uDMAboutBoxLogoStorrage;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Classes,
+  System.ImageList,
+  FMX.ImgList;
+
+type
+  TdmAboutBoxLogo = class(TDataModule)
+    imgLogo: TImageList;
+  private
+  public
+  end;
+
+implementation
+
+{%CLASSGROUP 'FMX.Controls.TControl'}
+{$R *.dfm}
+
+end.
